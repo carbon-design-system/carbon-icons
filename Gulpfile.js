9eac757f7d45fa19a5aced1f66a1d02a55d66d39
Stan Haozhe Li <lih@us.ibm.com>
2015-11-19 15:59:57 -0600

    Add icons used for CFS service dashboards Fixed bug in icons gulp task Generated new _icons.scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,13 +9,17 @@ var concat = require('gulp-concat');
 var svg = require('gulp-image-data-uri');
 
 gulp.task('icons', function() {
-  return gulp.src('svg/*.svg')
+  return gulp.src('svg/**/*.svg')
     .pipe(svg({
       customClass: function(className) {
         var customClass = 'icon--' + className;
         return customClass;
-      }
-    }))
+      },
+
+		template: {
+		    file: 'node_modules/gulp-image-data-uri/template.css'
+		}
+    } ))
     .pipe(concat('_icons.scss'))
     .pipe(gulp.dest('.'));
 });
