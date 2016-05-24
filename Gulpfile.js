82028ef895e2ec6601df7676d5689430fac740a2
BRIAN T. HAN <bthan@us.ibm.com>
2016-05-24 15:30:35 -0500

    Update gulpfile and add notification icons (#62)
    * Update gulpfile.js to use svgo
    
    * npm run build = gulp
    
    * ignore original-svg folder
    
    svg is now for optimized svg
    
    * Remove unused dependencies
    
    * Don't clean svg folder
    
    * Don't run svgo as part of default task
    
    * Create function for svgo instead
    
    * Add notification icons
    
    * Create seperate task for svgo
    
    * Clean up error.svg
    
    * Clean up info and success
    
    * Clean up warning svg
    
    * Add comments to svgOptimize()
    
    * sprite:rename-move for html and svg rename/move related tasks
    
    * Remove run-sequence, use npm scripts for task order
    
    * Remove standalone svgo task, move it into build task
    
    * Remove svgo script from package.json
    
    * Remove prebuild script
    
    Using && should run gulp tasks in sequence
    
    * Re-add dev script

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,9 +7,8 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
-var rsp = require('remove-svg-properties').stream;
 var del = require('del');
-var deploy = require('gulp-gh-pages');
+var svgo = require('gulp-svgo');
 
 // SVG Config
 var config = {
@@ -35,46 +34,35 @@ var config = {
   }
 };
 
+// Clean folders and files
 gulp.task('clean', () => {
   return del(['build', 'index.html', 'sprite.{css,svg}']);
 });
 
+// Build svg sprite
 gulp.task('build', function() {
   return gulp.src('svg/**/*.svg')
+    .pipe(svgo())
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('sprite-page', function() {
-  return gulp.src('build/symbol/*.{html,css}')
+// Create sprite.svg HTML page
+gulp.task('rename-move', function() {
+  return gulp.src('build/symbol/**/*.{html,css,svg}')
     .pipe(rename(function (path) {
       if (path.extname === '.html') {
         path.basename = 'index';
+      }
+
+      if (path.extname === '.svg') {
+        path.basename = 'sprite';
+        path.dirname = '.'
       }
     }))
     .pipe(gulp.dest('.'));
 });
 
-// renames a sprite shortcut for redundancy
-// removing fills and namespaces
-gulp.task('sprite-shortcut', function() {
-  return gulp.src('build/symbol/svg/sprite.symbol.svg')
-    .pipe(rename('sprite.svg'))
-    .pipe(rsp.remove({
-        properties: [rsp.PROPS_FILL],
-        namespaces: ['i', 'sketch', 'inkscape']
-    }))
-    .pipe(gulp.dest('.'));
-});
-
-// gulp.task('deploy', function() {
-//   return gulp.src('./*.{html,svg,css}')
-//     .pipe(deploy());
-// });
-
 gulp.task('default', function() {
-  console.warn(
-  `\n[ WARNING ]:
-  Use "npm run build".
-  This is to ensure gulp tasks run in order.\n`);
+  console.log('\nUse `npm run build` instead.\n');
 });
