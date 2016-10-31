0da5c9885441f054b73433a2a1e30e93cca59c88
BRIAN T. HAN <bthan@us.ibm.com>
2016-10-31 00:17:05 -0500

    Unit Tests (#169)
    * Add chalk
    
    * Remove jsdom
    
    * start tests for bluemix-icon files
    
    * Add tests for duplicate IDs
    
    * Add xml2js and remove test:watch
    
    * Set up tests for bluemix-icons.json
    
    * Finish tests for bluemix-icons files
    
    * Duplicate tests for legacy files
    
    * Add test for duplicate names in json
    
    * Update travis to test and install
    
    * Enable caching of node_modules and run build
    
    * Remove node 4
    
    * Add engines
    
    * Remove cache
    
    * Re-add cache

---
+++ __tests__/bluemix-icon.test.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+const bluemixIconsSvg = fs.readFileSync(`${__dirname}/../bluemix-icons.svg`).toString('utf8');
+const bluemixIconsJson = require('../bluemix-icons.json');
+// const spriteSvg = fs.readFileSync(`${__dirname}/../sprite.svg`).toString('utf8');
+
+const name = (svg) => {
+  return svg.getAttribute('id');
+}
+
+const warning = (svgName, message) => {
+  console.log(chalk.yellow(`
+    ${svgName}.svg:
+    * ${message}
+  `));
+}
+
+describe('bluemix-icons.svg', () => {
+  document.body.innerHTML = bluemixIconsSvg;
+  const symbols = [...document.querySelectorAll('symbol')];
+
+  it('should have fill-rule', () => {
+    symbols.forEach(symbol => {
+      if (!symbol.hasAttribute('fill-rule')) {
+        warning(name(symbol), 'fill-rule is missing');
+      }
+      expect(symbol.hasAttribute('fill-rule')).toBe(true);
+    });
+  });
+
+  it('should have [fill-rule="evenodd"]', () => {    
+    symbols.forEach(symbol => {
+      if (symbol.getAttribute('fill-rule') !== 'evenodd') {
+        warning(
+          name(symbol),
+          `fill-rule is: ${symbol.getAttribute('fill-rule')}\n\t...should be evenodd`);
+      }
+      expect(symbol.getAttribute('fill-rule')).toEqual('evenodd');
+    });
+  });
+
+  it('should not have duplicate id attributes', () => {
+    const sortedList = symbols.map(symbol => {
+      return symbol.getAttribute('id');
+    }).sort();
+
+    let results = [];
+
+    sortedList.forEach((id, index) => {
+      if (sortedList[index] === sortedList[index + 1]) {
+        results.push(sortedList[index]);
+      }
+    });
+
+    if (results.length > 0) {
+      console.log(chalk.yellow(`duplicate IDs: [${results}]`));
+    }
+    expect(results.length).toEqual(0);
+  });
+});
+
+describe('bluemix-icons.json', () => {
+  it('should have an id', () => {
+    bluemixIconsJson.forEach(object => {
+      expect(object.id).not.toEqual('');
+    })
+  })
+  it('should have a name', () => {
+    bluemixIconsJson.forEach(object => {
+      expect(object.name).not.toEqual('');
+    })
+  })
+  it('should have tags', () => {
+    bluemixIconsJson.forEach(object => {
+      expect(object.tags).not.toEqual('');
+    });
+  })
+  it('should have empty styles', () => {
+    bluemixIconsJson.forEach(object => {
+      expect(object.styles).toEqual('');
+    });
+  })
+  it('each svgData object should have a value', () => {
+    bluemixIconsJson.forEach(object => {
+      let dataCount = 0;
+      dataCount += object.svgData.circles.length;
+      dataCount += object.svgData.ellipses.length;
+      dataCount += object.svgData.paths.length;
+      dataCount += object.svgData.polygons.length;
+      dataCount += object.svgData.polylines.length;
+      dataCount += object.svgData.rects.length;
+      expect(dataCount).toBeGreaterThan(0);
+    });
+  });
+})
---
+++ __tests__/legacy.test.js
@@ -0,0 +1,115 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+const spriteSvg = fs.readFileSync(`${__dirname}/../sprite.svg`).toString('utf8');
+const iconJson = require('../icons.json');
+
+const name = (svg) => {
+  return svg.getAttribute('id');
+}
+
+const warning = (svgName, message) => {
+  console.log(chalk.yellow(`
+    ${svgName}.svg:
+    * ${message}
+  `));
+}
+
+describe('sprite.svg', () => {
+  document.body.innerHTML = spriteSvg;
+  const symbols = [...document.querySelectorAll('symbol')];
+
+  it('should have fill-rule', () => {
+    symbols.forEach(symbol => {
+      if (!symbol.hasAttribute('fill-rule')) {
+        warning(name(symbol), 'fill-rule is missing');
+      }
+      expect(symbol.hasAttribute('fill-rule')).toBe(true);
+    });
+  });
+
+  it('should have [fill-rule="evenodd"]', () => {    
+    symbols.forEach(symbol => {
+      if (symbol.getAttribute('fill-rule') !== 'evenodd') {
+        warning(
+          name(symbol),
+          `fill-rule is: ${symbol.getAttribute('fill-rule')}\n\t...should be evenodd`);
+      }
+      expect(symbol.getAttribute('fill-rule')).toEqual('evenodd');
+    });
+  });
+
+  it('should not have duplicate id attributes', () => {
+    const sortedList = symbols.map(symbol => {
+      return symbol.getAttribute('id');
+    }).sort();
+
+    let results = [];
+
+    sortedList.forEach((id, index) => {
+      if (sortedList[index] === sortedList[index + 1]) {
+        results.push(sortedList[index]);
+      }
+    });
+
+    if (results.length > 0) {
+      console.log(chalk.yellow(`duplicate IDs: [${results}]`));
+    }
+    expect(results.length).toEqual(0);
+  });
+});
+
+describe('icons.json', () => {
+  it('should have an id', () => {
+    iconJson.forEach(object => {
+      expect(object.id).not.toEqual('');
+    })
+  })
+  it('should have a name', () => {
+    iconJson.forEach(object => {
+      expect(object.name).not.toEqual('');
+    })
+  })
+  it('should have tags', () => {
+    iconJson.forEach(object => {
+      expect(object.tags).not.toEqual('');
+    });
+  })
+  it('should have empty styles', () => {
+    iconJson.forEach(object => {
+      expect(object.styles.toString()).toEqual('');
+    });
+  })
+  it('each svgData object should have a value', () => {
+    iconJson.forEach(object => {
+      let dataCount = 0;
+      dataCount += object.svgData.circles.length;
+      dataCount += object.svgData.ellipses.length;
+      dataCount += object.svgData.paths.length;
+      dataCount += object.svgData.polygons.length;
+      dataCount += object.svgData.polylines.length;
+      dataCount += object.svgData.rects.length;
+      expect(dataCount).toBeGreaterThan(0);
+    });
+  });
+
+  it('should not have duplicate name values', () => {
+    const sortedList = iconJson.map(object => {
+      return object.name;
+    }).sort();
+
+    let results = [];
+
+    sortedList.forEach((name, index) => {
+      if (sortedList[index] === sortedList[index + 1]) {
+        results.push(sortedList[index]);
+      }
+    });
+
+    if (results.length > 0) {
+      console.log(chalk.yellow(`duplicate names: [${results}]`));
+    }
+    expect(results.length).toEqual(0);
+  })
+})
+
