20fc3ba29ded0b64dc97ae0a1ff9e7febcb5ffc0
bthan <bthan@us.ibm.com>
2017-03-25 15:56:08 -0500

    chore(gulp-clean): remove old files from del list

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -4,8 +4,10 @@ const del = require('del');
 // Clean folders and files
 const clean = () => {
   return del([
-    'dist', 'build', 'index.html', 'sprite.{css,svg}', 'icons.json', `legacy-icons.js`,
-    'sprites', 'spriteSVGIndex.html', 'bluemix-icons.{json,svg,js}'
+    'dist',
+    'index.html',
+    'sprites',
+    'carbon-icons.{json,svg,js}',
   ]);
 }
 
