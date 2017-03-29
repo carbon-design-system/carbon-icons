86db359171bc8791b456cfc0623adc7d4fab8f32
bthan <bthan@us.ibm.com>
2017-03-29 15:13:29 -0500

    refactor(gulp-tasks): remove xmlns

--- gulp-tasks/build-svg-dist.js
+++ gulp-tasks/build-svg-dist.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const svgBuild = require('./shared/build');
+const dom = require('gulp-dom');
 
 const buildSvgDist = () => {
   svgBuild('src/svg/*.svg')
+    .pipe(dom(function () {
+      this.querySelector('svg').removeAttribute('xmlns');
+      return this.querySelector('body').innerHTML
+    }, false))
     .pipe(gulp.dest('dist/svg'));
 }
 
