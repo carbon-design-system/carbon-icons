107612e365c0a2ab7282503c22727933757fbd3a
Brian Han <hellobrian@users.noreply.github.com>
2017-05-25 14:20:22 -0500

    chore(package.json): change mainfile to dist/carbon-icons.js (#28)
    * chore(copy.js): remove commented code
    
    * chore(bluemix-files): remove bluemix-named files
    
    * chore(package.json): change main file to dist/carbon-icons

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -2,44 +2,35 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 
 function copySvg() {
-  return gulp.src('dist/sprites/symbol/**/*.svg')
-    .pipe(rename(function (path) {
-      if (path.extname === '.svg') {
-        path.basename = 'carbon-icons';
-        path.dirname = '.'
-      }
-    }))
+  return gulp
+    .src('dist/sprites/symbol/**/*.svg')
+    .pipe(
+      rename(function(path) {
+        if (path.extname === '.svg') {
+          path.basename = 'carbon-icons';
+          path.dirname = '.';
+        }
+      })
+    )
     .pipe(gulp.dest('dist'));
 }
 
-
 function copyHtml() {
-  return gulp.src('dist/sprites/symbol/**/*.{html,css}')
-    .pipe(rename(function (path) {
-      if (path.extname === '.html') {
-        path.basename = 'index';
-      }
-    }))
+  return gulp
+    .src('dist/sprites/symbol/**/*.{html,css}')
+    .pipe(
+      rename(function(path) {
+        if (path.extname === '.html') {
+          path.basename = 'index';
+        }
+      })
+    )
     .pipe(gulp.dest('dist'));
 }
 
 const copy = () => {
   copySvg();
   copyHtml();
-  // gulp.src('dist/sprites/symbol/**/*.{html,css,svg}')
-  //   .pipe(rename(function (path) {
-  //     if (path.extname === '.svg') {
-  //       path.basename = 'carbon-icons';
-  //       path.dirname = '.'
-  //     }
-  //   }))
-  //   .pipe(gulp.dest('.'))
-  //   .pipe(rename(function (path) {
-  //     if (path.extname === '.html') {
-  //       path.basename = 'index';
-  //     }
-  //   }))
-  //   .pipe(gulp.dest('dist'));
-}
+};
 
 module.exports = copy;
