5984bca64dfc875a774683516425161942a92647
Brian Han <hellobrian@users.noreply.github.com>
2017-06-22 01:27:29 -0500

    build(gulp): update build.js to convert circles to paths (#33)
    build(gulp): update build.js to convert circles to paths

--- gulp-tasks/build-sprite.js
+++ gulp-tasks/build-sprite.js
@@ -6,21 +6,20 @@ const svgBuild = require('./shared/build');
 const config = {
   shape: {
     id: {
-      generator: "icon--%s"
-    }
+      generator: 'icon--%s',
+    },
   },
   mode: {
-    symbol: { // Activate the defs mode
+    symbol: {
+      // Activate the defs mode
       bust: false, // Cache busting
-      example: true // Build a page
-    }
-  }
+      example: true, // Build a page
+    },
+  },
 };
 
 const buildSprite = () => {
-  svgBuild('src/svg/*.svg')
-    .pipe(svgSprite(config))
-    .pipe(gulp.dest('./dist/sprites'));
-}
+  svgBuild('src/svg/*.svg').pipe(svgSprite(config)).pipe(gulp.dest('./dist/sprites'));
+};
 
 module.exports = buildSprite;
